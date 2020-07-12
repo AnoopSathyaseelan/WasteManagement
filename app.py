198ef68c8d2f3386a2a210d35968c0b89ebc4705
AnoopSathyaseelan <anoopsathyaseelan@gmail.com>
2020-07-12 16:35:08 +0530

    commit

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app=Flask(__name__)
 
-@app.route('/<string:name>')
+@app.route('/')
 def hello(name):
-    return  "hello "+name
+    return  "hello world"
 
 if __name__=="__main__":
     app.run(debug=True)    
